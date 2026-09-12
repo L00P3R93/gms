004310e008c090bdfc87a2fcb5b68e73542b710c
Vincent Kioko <vincentkioko51@gmail.com>
2026-09-12 10:09:38 +0300

    Update config/logging.php

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'mpesa' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/mpesa.log'),
+            'level' => 'info',
+        ],
+
     ],
 
 ];
